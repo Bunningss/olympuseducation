0988d81851e1889ed0f34661248dbc1ef4a2a33d
bunningss <mahmudovi183@gmail.com>
2023-12-23 02:01:39 +0600

    footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,44 @@
+import Image from "next/image";
 import Container from "../Container/Container";
 import styles from "./Footer.module.css";
+import { roboto } from "@/fonts/fonts";
+
+import facebook from "../../../public/images/icons/facebook.png";
+import instagram from "../../../public/images/icons/instagram.png";
+import youtube from "../../../public/images/icons/youtube.png";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
+    <div className={`${roboto.className} ${styles.footer}`}>
       <Container>
         <div className={styles.footerWrapper}>
           <div className={styles.content}>
-            <h4>Want to talk to us?</h4>
-            <h2>
+            <h4 className={styles.footerTitle}>Want to talk to us?</h4>
+            <h2 className={styles.footerLink}>
               <a href="mailto:olympus@gmail.com">olympus@gmail.com</a>
             </h2>
-            {/* Location */}
-            <div className={styles.location}></div>
+            <h2 className={styles.footerLink}>
+              <a href="mailto:olympus@gmail.com">+8801873228724</a>
+            </h2>
+            {/* Footer Bottom */}
+            <div className={styles.footerBottom}>
+              {/* Location */}
+              <div className={styles.footerBottomContentWrapper}>
+                <h4 className={styles.footerBottomHeader}>We are here</h4>
+                <p className={styles.footerBottomText}>
+                  Marine Drive, Sylhet, Bangladesh.
+                </p>
+              </div>
+              {/* Social */}
+              <div className={styles.footerBottomContentWrapper}>
+                <h4 className={styles.footerBottomHeader}>follow us</h4>
+                <div className={styles.socialLinks}>
+                  <Image className={styles.socialIcon} src={facebook} alt="" />
+                  <Image className={styles.socialIcon} src={instagram} alt="" />
+                  <Image className={styles.socialIcon} src={youtube} alt="" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,6 +1,9 @@
 import { Nunito_Sans, Roboto_Slab } from "next/font/google";
 
-export const roboto = Roboto_Slab({ subsets: ["cyrillic"], weight: ["900"] });
+export const roboto = Roboto_Slab({
+  subsets: ["cyrillic"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export const nunito = Nunito_Sans({
   weight: ["300", "400", "500", "700"],
