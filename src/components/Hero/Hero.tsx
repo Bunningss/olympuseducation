e28c8282648d2a746ef8a94e4ca0b0c9b0b09e12
bunningss <mahmudovi183@gmail.com>
2024-02-18 23:29:33 +0600

    minor improvements

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -19,6 +19,12 @@ const Hero = () => {
           <div className={styles.heroSocialContainer}>
             <Social />
           </div>
+          {/* Mouse Scroll */}
+          <div className={styles.scrollDown}>
+            <div className={styles.mousey}>
+              <div className={styles.scroller}></div>
+            </div>
+          </div>
         </div>
       </Container>
     </Section>
