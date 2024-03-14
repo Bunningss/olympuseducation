5f6a931bc1475466917cc955d9f0b5039cc182b4
bunningss <mahmudovi183@gmail.com>
2024-03-14 23:15:43 +0600

    landscape mode

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,10 +7,3 @@ export function setCookie(name: string, value: {}) {
 
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
-
-export function getCookie(name: string) {
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookie = decodedCookie.split("=")[1];
-
-  return cookie && JSON.parse(cookie);
-}
